aaabe22891dbe029b1ca7ede4eec17863e3c2417
xiaolongren <52064463+Little-Dragon-God@users.noreply.github.com>
2021-06-20 23:44:25 +0800

    双重检查，线程安全

--- src/com/s/singleton/Singleton6.java
+++ src/com/s/singleton/Singleton6.java
@@ -6,7 +6,9 @@ public class Singleton6 {
     public static Singleton6 newInstance() throws InterruptedException {
         //双重检查,是单例
         if (instance == null){
+            //首先判断实例是否为空，空就上锁
             synchronized (Singleton6.class){
+                //上锁后，如果上面new出了个对象，此时在这判断是否为空，不为空就直接返回了，确保了只有一个实例
                 if (instance == null){
                     Thread.sleep(20);
                     instance = new Singleton6();
