e81c7254065fdb5f69a8a3544163f956a73457be
xiaolongren <52064463+Little-Dragon-God@users.noreply.github.com>
2021-06-24 21:30:26 +0800

    序列化实现深拷贝

--- src/com/s/prototype/deep/Client.java
+++ src/com/s/prototype/deep/Client.java
@@ -3,12 +3,14 @@ package com.s.prototype.deep;
 public class Client {
     public static void main(String[] args) throws Exception {
         DeepProtoType p = new DeepProtoType();
-        p.name = "宋江";
+        p.name = "小黑子";
         p.deepCloneableTarget = new DeepCloneableTarget("大牛", "小牛");
         //方式 1  完成深拷贝哈希码不一致
-        DeepProtoType p2 = (DeepProtoType) p.clone();
-        System.out.println("p.name=" + p.name + p.deepCloneableTarget+"p.deepCloneableTarget=" + p.deepCloneableTarget.hashCode());
-        System.out.println("p2.name=" + p2.name + p2.deepCloneableTarget+"p2.deepCloneableTarget=" + p2.deepCloneableTarget.hashCode());
+        //DeepProtoType p2 = (DeepProtoType) p.clone();
+        //方式 2  完成深拷贝
+        DeepProtoType p2 = (DeepProtoType) p.deepClone();
+        System.out.println("p.name=" + p.name + p.deepCloneableTarget + p.deepCloneableTarget.hashCode());
+        System.out.println("p2.name=" + p2.name + p2.deepCloneableTarget + p2.deepCloneableTarget.hashCode());
 
     }
 }
--- src/com/s/prototype/deep/DeepProtoType.java
+++ src/com/s/prototype/deep/DeepProtoType.java
@@ -1,14 +1,15 @@
 package com.s.prototype.deep;
 
-import java.io.Serializable;
+import java.io.*;
 
-public class DeepProtoType implements Serializable,Cloneable {
+public class DeepProtoType implements Serializable, Cloneable {
     public String name;
     public DeepCloneableTarget deepCloneableTarget;// 引用类型
 
     public DeepProtoType() {
         super();
     }
+
     //深拷贝方式1:使用 clone 方法
     @Override
     protected Object clone() throws CloneNotSupportedException {
@@ -20,4 +21,40 @@ public class DeepProtoType implements Serializable,Cloneable {
         deepProtoType.deepCloneableTarget = (DeepCloneableTarget) deepCloneableTarget.clone();
         return deepProtoType;
     }
+
+    //深拷贝方式2:序列化实现深拷贝（推荐）
+    public Object deepClone()  {
+        ByteArrayOutputStream bos = null;
+        ObjectOutputStream oos = null;
+        ByteArrayInputStream bis = null;
+        ObjectInputStream ois = null;
+
+            //序列化
+        DeepProtoType copyObj = null;
+        try {
+            bos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(this); //当前这个对象以对象流的方式输出
+
+            //反序列化
+            bis = new ByteArrayInputStream(bos.toByteArray());
+            ois = new ObjectInputStream(bis);
+            copyObj = (DeepProtoType)ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bos.close();
+                oos.close();
+                bis.close();
+                ois.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return copyObj;
+    }
+
 }
